14719e9c83114884e1634deeab3ceef6481df506
guillaumeyvo <guillaumeyvo@yahoo.fr>
2016-03-06 21:48:37 +0100

    App
    Push des views initiales

---
+++ DlcApp/app/src/main/java/com/app/dlc/dlc/Distributeur_action.java
@@ -0,0 +1,13 @@
+package com.app.dlc.dlc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Distributeur_action extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distributeur_action);
+    }
+}
---
+++ DlcApp/app/src/main/java/com/app/dlc/dlc/Signup_user.java
@@ -0,0 +1,13 @@
+package com.app.dlc.dlc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signup_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_user);
+    }
+}
