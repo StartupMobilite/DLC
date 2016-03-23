99570a7bf66d881db8b074d4f699bc17c11a43db
guillaumeyvo <guillaumeyvo@yahoo.fr>
2016-03-23 01:29:19 +0100

    Lien entre les activites authentication et search

--- DlcApp/app/src/main/java/com/app/dlc/dlc/Authentication.java
+++ DlcApp/app/src/main/java/com/app/dlc/dlc/Authentication.java
@@ -1,7 +1,12 @@
 package com.app.dlc.dlc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.*;
+import android.content.Intent;
 
 public class Authentication extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class Authentication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+        Button btidentifier = (Button) findViewById(R.id.btindentifier); // obtenir la reference sur le button identifier
+
+        btidentifier.setOnClickListener(new View.OnClickListener() { // on attribue un click listener sur le button identifier
+            public void onClick(View v) {
+                //on cree un intent. Un intent est en gros le moyen utliser par android pour passer d une activite a une autre
+                // on definit l intent de la maniere suivante de l activite actuelle vers l activite search
+                Intent intent = new Intent(Authentication.this,Search.class);
+                Authentication.this.startActivity(intent); // enclenche l intent
+            }
+        });
+
+
     }
 }
